5280ac80a844c7b072488f016bfc226c30b6aeaf
cesaraugustomt <contatocsaugusto@gmail.com>
2023-09-06 16:51:34 -0300

    :tada: Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {SafeAreaView, Text} from 'react-native';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Primeiro app</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
