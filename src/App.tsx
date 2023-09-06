b074ac25d548ef0826d67242ed1b9f4733385391
cesaraugustomt <contatocsaugusto@gmail.com>
2023-09-06 18:03:54 -0300

    feat: adiciona styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 48px;
+  color: #7f13ba;
+`;
+
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Primeiro app</Text>
+      <TextNew>Novo teste</TextNew>
     </SafeAreaView>
   );
 };
