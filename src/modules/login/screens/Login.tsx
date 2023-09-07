b7e437231e93ee8f97fe9a3db0ef008482e95f40
cesaraugustomt <contatocsaugusto@gmail.com>
2023-09-07 14:59:20 -0300

    feat: adiciona layout button

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/Input';
 import Text from '../../../shared/components/text/Text';
+import { theme } from '../../../shared/themes/theme';
 import { ContainerLogin } from '../styles/login.style';
 
 const Login = () => {
@@ -15,7 +16,12 @@ const Login = () => {
       <ContainerLogin>
         <Text>test</Text>
         <Input />
-        <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
+        <Button
+          type={theme.buttons.buttonsTheme.primary}
+          margin="16px"
+          title="ENTRAR"
+          onPress={handleOnPress}
+        />
       </ContainerLogin>
     </View>
   );
--- src/shared/themes/colors.ts
+++ src/shared/themes/colors.ts
@@ -4,3 +4,15 @@ export const grayTheme = {
 export const neutralTheme = {
   white: '#ffffff',
 };
+
+export const mainTheme = {
+  primary: '#8C29B0',
+};
+
+export const pinkTheme = {
+  pink80: '#C833DB',
+};
+
+export const purpleTheme = {
+  purple80: '#512C85',
+};
