f3f41ccd322b910f89da6d78d38debd2213b1d0b
cesaraugustomt <contatocsaugusto@gmail.com>
2023-09-13 15:47:40 -0300

    feat: adiciona form para registrar usuario

--- src/modules/createUser/screens/CreateUser.tsx
+++ src/modules/createUser/screens/CreateUser.tsx
@@ -1,17 +1,61 @@
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/Input';
+import { useCreateUser } from '../hooks/useCreateUser';
 import { CreateUserContainer } from '../styles/createUser.style';
 
 const CreateUser = () => {
+  const { createUser, handleOnChangeInput, handleCreateUser, loading } = useCreateUser();
+
   return (
     <CreateUserContainer>
-      <Input margin="0px 0px 16px 0px" placeholder="digite" title="Nome Completo:" />
-      <Input margin="0px 0px 16px 0px" placeholder="digite" title="Telefone:" />
-      <Input margin="0px 0px 16px 0px" placeholder="digite" title="Email:" />
-      <Input margin="0px 0px 16px 0px" placeholder="digite" title="CPF:" />
-      <Input margin="0px 0px 16px 0px" placeholder="digite" title="Senha:" />
-      <Input margin="0px 0px 16px 0px" placeholder="digite" title="Confirmar Senha:" />
-      <Button margin="0px 0px 32px 0px" title="Criar Usuário" />
+      <Input
+        value={createUser.name}
+        onChange={(event) => handleOnChangeInput(event, 'name')}
+        margin="0px 0px 16px 0px"
+        placeholder="digite"
+        title="Nome Completo:"
+      />
+      <Input
+        value={createUser.phone}
+        onChange={(event) => handleOnChangeInput(event, 'phone')}
+        margin="0px 0px 16px 0px"
+        placeholder="digite"
+        title="Telefone:"
+      />
+      <Input
+        value={createUser.email}
+        onChange={(event) => handleOnChangeInput(event, 'email')}
+        margin="0px 0px 16px 0px"
+        placeholder="digite"
+        title="Email:"
+      />
+      <Input
+        value={createUser.cpf}
+        onChange={(event) => handleOnChangeInput(event, 'cpf')}
+        margin="0px 0px 16px 0px"
+        placeholder="digite"
+        title="CPF:"
+      />
+      <Input
+        value={createUser.password}
+        onChange={(event) => handleOnChangeInput(event, 'password')}
+        margin="0px 0px 16px 0px"
+        placeholder="digite"
+        title="Senha:"
+      />
+      <Input
+        value={createUser.confirmPassword}
+        onChange={(event) => handleOnChangeInput(event, 'confirmPassword')}
+        margin="0px 0px 16px 0px"
+        placeholder="digite"
+        title="Confirmar Senha:"
+      />
+      <Button
+        onPress={handleCreateUser}
+        loading={loading}
+        margin="0px 0px 32px 0px"
+        title="Criar Usuário"
+      />
     </CreateUserContainer>
   );
 };
